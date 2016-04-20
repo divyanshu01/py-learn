086233f8df12df58f63b750fa600fbd0758d5ea0
dragon <divyanshu_j01@yahoo.in>
2016-04-20 12:40:08 +0530

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -1,17 +1,17 @@
 print "so your going on a vacation eh?"
 print "well you need me"
-cav = str(raw_input("are you going in a coach or first class?:"))
-if cav == "coach":
-    scav = str(raw_input("are you going to be in a penthouse or hotel:"))
-    if scav == "hotel":
-        print "your trip is possible and it costs 1000$"
-    if scav == "pentous":
-        print "your trip is possible and it costs 2000$"
+cav = input("are you going in a coach or first class?\n")
+if cav.lower == "coach":
+    scav = input("are you going to be in a penthouse or hotel:")
+    if scav.lower == "hotel":
+        print("your trip is possible and it costs 1000$")
+    if scav.lower == "pentous":
+        print("your trip is possible and it costs 2000$")
 
-if cav == "first class":
-    scav = str(raw_input("are you going to be in a penthouse or hotel:"))
-    if scav == "hotel":
-        print "your trip is possible and it costs 2000$"
-    if scav == "pentous":
-        print "your trip is possible and it costs 3000$"
+elif cav.lower == "first class":
+    scav = input("are you going to be in a penthouse or hotel:")
+    if scav.lower == "hotel":
+        print("your trip is possible and it costs 2000$")
+    if scav.lower == "pentous":
+        print("your trip is possible and it costs 3000$")
 
