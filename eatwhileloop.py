1092942835f4a9a007f54c813277a8e3a73c5242
dragon <divyanshu_j01@yahoo.in>
2016-05-09 17:42:32 +0530

    Update eatwhileloop.py

--- eatwhileloop.py
+++ eatwhileloop.py
@@ -1,11 +1,11 @@
 import time
 
-print "you will be ...."
+print("you will be ....")
 
 count = 8
-while (0< count < 9):
+while (0 < count < 9):
     print count
     count = count - 1
     time.sleep(1)
 
-print "eaten!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+print("eaten!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
