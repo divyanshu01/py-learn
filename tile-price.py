6b8ea4df041e278907cd8f0661249350cf96ad44
dragon <divyanshu_j01@yahoo.in>
2016-05-09 17:46:21 +0530

    Update tile-price.py

--- tile-price.py
+++ tile-price.py
@@ -1,5 +1,5 @@
-lnt = int(raw_input('enter-the-lenght-of-the-floor-in-square-feet.:'))
-wth = int(raw_input('enter-the-with-of-the-floor-in-square-feet.:'))
-price = int(raw_input("enter-the-price-of-a-square-foot-of-tiling-without-the-dollar-sign:"))
+lnt = input('enter-the-lenght-of-the-floor-in-square-feet.:')
+wth = input('enter-the-with-of-the-floor-in-square-feet.:')
+price = input("enter-the-price-of-a-square-foot-of-tiling-without-the-dollar-sign:")
 a = str(lnt * wth * price)
-print "the floor costs " + a + " dollars"
+print("the floor costs " + a + " dollars")
